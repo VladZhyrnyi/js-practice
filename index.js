0f6d600603ae734e4825d31009ed8890b48559f3
VladZhyrnyi <vladzhyrnyi@ukr.net>
2022-12-17 21:37:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -83,19 +83,127 @@
 
 // console.log(result);
 
-let number = prompt("Ведіть число");
-let total = 0;
+// let number = prompt("Ведіть число");
+// let total = 0;
 
 // do {
 //   total += Number(number);
 //   number = prompt("Ведіть число");
 // } while (number === true);
 
-while (number) {
-  total += Number(number);
-  number = prompt("Ведіть число");
+// while (number) {
+//   total += Number(number);
+//   number = prompt("Ведіть число");
 
-  console.log(total);
+//   console.log(total);
+// }
+
+// console.log(total);
+
+
+// ___ Practice 12.17.22 ----------------------------------------------------------------
+
+// 1 Створити метод об'єкту який буде приймати 1 параметр назву факультету та повертати списoк імен учнів факультету
+// 2 Створити метод об'єкту який буде приймати 1 параметр назву факультету та повертати кількість очків факультета
+// 3 Створити метод об'єкту який буде виводити ім'я факультету в якого більше очків
+
+// const hogvarts = {
+//     griffindor: [{
+//             name: "Harry",
+//             points: 17
+//         },
+//         {
+//             name: "Hermiona",
+//             points: 19
+//         },
+//         {
+//             name: "Ron",
+//             points: 14
+//         },
+//     ],
+//     sliserin: [{
+//             name: "Draco",
+//             points: 117
+//         },
+//         {
+//             name: "Goyl",
+//             points: 14
+//         },
+//         {
+//             name: "Crabbe",
+//             points: 5
+//         },
+//     ],
+
+//     getPoints(faculty) {
+//       let sum = 0;
+//       for (const {points} of this[faculty]) {
+//         sum += points;
+//       }
+//       return sum;
+//     },
+
+//     getBest() {
+//       let name = '';
+//       let bestPoints = 0;
+
+//       const keys = Object.keys(this);
+//       for (const key of keys) {
+//         if (typeof this[key] === 'object' && this.getPoints(key) > bestPoints) {
+//           bestPoints = this.getPoints(key);
+//           name = key;
+//         }
+//         // console.log(typeof this[key]);
+//       }
+//       return name;
+//     }
+// }
+
+
+// console.log(hogvarts.getBest())
+
+
+// ___ CodeWars: count dublicates in string ----------------------------------------------------
+
+// function duplicateCount(text) {
+//   const letterArray = text.split('');
+//   const dublicates = [];
+//   for (let i=0; i<letterArray.length; i++) {
+//     for (let j=i+1; j<letterArray.length; j++) {
+//       if (letterArray[i] === letterArray[j] && !dublicates.includes(letterArray[i])) {
+//         dublicates.push(letterArray[i]);
+//       }
+//     }
+//   }
+//   return dublicates.length
+// }
+
+// console.log(duplicateCount('aabbccabde'))
+
+
+
+// ___ CodeWars: sum of interval ---------------------------------------------------------------
+
+function getSum( a,b ) {
+   //Good luck!
+  // let sum = 0;
+  // if (a === b) {
+  //   return 0;
+  // } else if (a < b) {
+  //   for (let i=a; i<=b; i++) {
+  //   sum += i;
+  //   } 
+  // } else {
+  //   for (let i=b; i<=a; i++) {
+  //   sum += i;
+  //   }
+  // }
+  // return sum;
+
+  min = Math.min(...arguments);
+  max = Math.max(...arguments); 
+
+  return (max - min + 1) * ((max - min) / 2)
 }
 
-console.log(total);
+console.log(getSum(0, 10));
