42d2e779682f8cf8f71d89260a8ed710427ae5d6
VladZhyrnyi <vladzhyrnyi@ukr.net>
2023-01-14 11:14:40 +0200

    1

--- index.js
+++ index.js
@@ -100,7 +100,6 @@
 
 // console.log(total);
 
-
 // ___ Practice 12.17.22 ----------------------------------------------------------------
 
 // 1 Створити метод об'єкту який буде приймати 1 параметр назву факультету та повертати списoк імен учнів факультету
@@ -159,9 +158,7 @@
 //     }
 // }
 
-
 // console.log(hogvarts.getBest())
-
 
 // ___ CodeWars: count dublicates in string ----------------------------------------------------
 
@@ -180,30 +177,252 @@
 
 // console.log(duplicateCount('aabbccabde'))
 
-
-
 // ___ CodeWars: sum of interval ---------------------------------------------------------------
 
-function getSum( a,b ) {
-   //Good luck!
-  // let sum = 0;
-  // if (a === b) {
-  //   return 0;
-  // } else if (a < b) {
-  //   for (let i=a; i<=b; i++) {
-  //   sum += i;
-  //   } 
-  // } else {
-  //   for (let i=b; i<=a; i++) {
-  //   sum += i;
-  //   }
-  // }
-  // return sum;
+// function getSum( a,b ) {
+//    //Good luck!
+//   // let sum = 0;
+//   // if (a === b) {
+//   //   return 0;
+//   // } else if (a < b) {
+//   //   for (let i=a; i<=b; i++) {
+//   //   sum += i;
+//   //   }
+//   // } else {
+//   //   for (let i=b; i<=a; i++) {
+//   //   sum += i;
+//   //   }
+//   // }
+//   // return sum;
 
-  min = Math.min(...arguments);
-  max = Math.max(...arguments); 
+//   //---//---//---//---//---//---// V2 //---//---//---//---
 
-  return (max - min + 1) * ((max - min) / 2)
-}
+//   const min = Math.min(...arguments);
+//   const max = Math.max(...arguments);
 
-console.log(getSum(0, 10));
+//   return (max - min + 1) * ((max - min) / 2)
+// }
+
+// console.log(getSum(0, 11));
+
+
+
+
+
+
+
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// // Change code below this line
+
+// const evenNumbers = numbers.filter((number) => number / 2 === 0);
+// const oddNumbers = numbers.filter((number) => number / 2 !== 0);
+
+// console.log(oddNumbers);
+
+
+
+
+
+
+
+
+// const players = [
+//   { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+//   { name: "Poly", playtime: 469, gamesPlayed: 2 },
+//   { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+//   { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+// ];
+// // Change code below this line
+
+// const totalAveragePlaytimePerGame = players.reduce(
+//   (total, { playtime, gamesPlayed }) => (total += playtime / gamesPlayed)
+// );
+
+// console.log(totalAveragePlaytimePerGame)
+
+
+
+
+
+const users = 
+[
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male",
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female",
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male",
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female",
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male",
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male",
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female",
+  },
+];
+
+// const getTotalFriendCount = (users) => users.reduce((total, user) => [...total, ...user.friends], []).length;
+
+// console.table(getTotalFriendCount(users))
+
+
+
+
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+// // Change code below this line
+
+// const sortedByAuthorName = [...books].sort((a, b) => a.author - b.author);
+
+// const sortedByReversedAuthorName = [...books].sort((a, b) => b.author - a.author);
+
+// const sortedByAscendingRating = [...books].sort((a, b) => a.rating - b.rating);
+
+// const sortedByDescentingRating = [...books].sort((a, b) => b.rating - a.rating);
+
+// console.log(sortedByAuthorName)
+
+
+
+
+
+
+// const getSortedFriends = (users) =>
+//   users
+//     .reduce((total, user) => [...total, ...user.friends], [])
+//     .filter((friend, index) => users.indexOf(friend) === index)
+//     .sort();
+
+// console.log(getSortedFriends(users));
+
+
+
+
+// const getTotalBalanceByGender = (users, gender) =>
+//   users
+//     .filter((user) => user.gender === gender)
+//     .reduce((total, user) => (total += user.balance), 0);
+
+
+// console.log(getTotalBalanceByGender(users, 'female'))
+
+
+
+
+// class Car {
+//   static #MAX_PRICE = 50000;
+//   // Change code below this line
+
+//   static checkPrice(price) {
+//     console.log(price)
+//     console.log(price > this.#MAX_PRICE)
+//     return price > this.#MAX_PRICE
+//       ? "Error! Price exceeds the maximum"
+//       : "Success! Price is within acceptable limits";
+//   }
+
+//   // Change code above this line
+//   constructor({ price }) {
+//     this.price = price;
+//   }
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); 
+// console.log(Car.checkPrice(bmw.price)); 
+
+
+
+// _________________Codewars____________________
+
+// function solution(str) {
+//   if (str.length % 2 === 1) {
+//     str += "_";
+//   }
+
+//   const result = [];
+
+//   for (let i = 0; i < str.length; i += 2) {
+//     result.push(str[i] + str[i + 1])
+//   };
+
+//   return result;
+// }
+
+// console.log(solution("abcdhgvfdcxsdaffjbfjbkgef"));
+
+// const getAllFriends = users => users.flatMap(user => user.friends)
+
+// console.log(getAllFriends(users));
+
+console.dir(document)
